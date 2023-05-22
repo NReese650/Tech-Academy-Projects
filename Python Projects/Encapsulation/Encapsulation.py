7b140e1467fe7c37d4d63eb9bbee7c8ae08a0971
NReese650 <128111600+NReese650@users.noreply.github.com>
2023-05-22 12:37:46 -0700

    Encapsulation
    Completing encapsulation assignment

---
+++ Python Projects/Encapsulation/Encapsulation.py
@@ -0,0 +1,70 @@
+class EncapsulationExample:
+    def __init__(self):
+        self.public_attribute = "Public Attribute"
+        self._protected_attribute = "Protected Attribute"
+        self.__private_attribute = "Private Attribute"
+
+    def public_method(self):
+        """
+        Public method can be accessed from outside the class.
+        """
+        print("Public Method")
+
+    def _protected_method(self):
+        """
+        Protected method can be accessed within the class and its subclasses.
+        """
+        print("Protected Method")
+
+    def __private_method(self):
+        """
+        Private method can only be accessed within the class itself.
+        """
+        print("Private Method")
+
+    def use_attributes_and_methods(self):
+        """
+        Method to demonstrate the use of attributes and methods.
+        """
+        # Accessing public attribute
+        print(self.public_attribute)
+
+        # Accessing protected attribute
+        print(self._protected_attribute)
+
+        # Accessing private attribute
+        print(self.__private_attribute)
+
+        # Calling public method
+        self.public_method()
+
+        # Calling protected method
+        self._protected_method()
+
+        # Calling private method
+        self.__private_method()
+
+
+# Create an object of the EncapsulationExample class
+obj = EncapsulationExample()
+
+# Accessing public attribute
+print(obj.public_attribute)
+
+# Accessing protected attribute
+print(obj._protected_attribute)
+
+# Accessing private attribute 
+print(obj._EncapsulationExample__private_attribute)
+
+# Calling public method
+obj.public_method()
+
+# Calling protected method
+obj._protected_method()
+
+# Calling private method (Name mangling is applied)
+obj._EncapsulationExample__private_method()
+
+# Using the method to demonstrate the use of attributes and methods
+obj.use_attributes_and_methods()
