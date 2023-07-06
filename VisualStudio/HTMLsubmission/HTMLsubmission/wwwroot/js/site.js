83c7bf72b396bc289fcb33b3440ae46a8039e06b
NReese650 <128111600+NReese650@users.noreply.github.com>
2023-07-05 21:40:13 -0700

    Visual Studio
    Completing visual studio assignments

---
+++ VisualStudio/HTMLsubmission/HTMLsubmission/wwwroot/js/site.js
@@ -0,0 +1,17 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+
+// Get the video container element
+var videoContainer = document.getElementById('video-container');
+
+// Add event listeners for mouseover and mouseout
+videoContainer.addEventListener('mouseover', function () {
+    videoContainer.style.backgroundColor = 'blue';
+});
+
+videoContainer.addEventListener('mouseout', function () {
+    videoContainer.style.backgroundColor = '';
+});
